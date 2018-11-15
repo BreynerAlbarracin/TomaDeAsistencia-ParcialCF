fff3ef2f6f0def791c09d2022618f39d99b26b95
usaka rokujou <usaka_rokujou@outlook.com>
2018-11-15 18:58:53 -0500

    Terminado la programación de la caja

--- Arduino/RFIDv1/RFIDv1.ino
+++ Arduino/RFIDv1/RFIDv1.ino
@@ -1,93 +1,231 @@
-
-/* Typical pin layout used:
-  -------------------------------------------------
-              MFRC522      Arduino       Arduino
-              Reader/PCD   Uno/101       Mega
-  Signal      Pin          Pin           Pin
-  -------------------------------------------------
-  RST/Reset   RST          9             5
-  SPI SS      SDA(SS)      10            53
-  SPI MOSI    MOSI         11 / ICSP-4   51
-  SPI MISO    MISO         12 / ICSP-1   50
-  SPI SCK     SCK          13 / ICSP-3   52
+/*
+   D0
+   D1 SDA (LDC)
+   D2 SCL
+   D3
+   D4 SDA
+   D5 SCK
+   D6 MISO
+   D7 MOSI
+   D8 RST
 */
 
+#include <ESP8266WiFi.h>
 #include <SPI.h>
+#include <ESP8266HTTPClient.h>
 #include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
-#define RST_PIN         5          // Configurable, segun el arduino
-#define SS_PIN          53         // Configurable, segun el arduino
+//WiFi
+const char* ssid = ":v";
+const char* password = "Usaka.red2804";
+const int httpPort = 80;
 
-#define ledPinVerde     8          // Led Verde - Llego a tiempo
-#define ledPinAzul      9          // Led Azul - Llego tarde
-#define ledPinRojo      10         // Led rojo - Clase equivocada
+//RFID
+#define SDA D4
+#define RST D8
+MFRC522 mfrc522(SDA, RST);  // Create MFRC522 instance
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Crea objeto MFRC522
+//Pantalla
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int numero_serie[5]; // Número de serie de la tarjeta que nuestro sensor RFID detectará
-
-#define AUTHORIZED_COUNT 2 //Para autoriazar más tarjetas ponemos el número aqui y la añadimos abajo
-byte Authorized[AUTHORIZED_COUNT][6] = {
-
-  {0x13, 0x2C, 0xF9, 0x95}, {0x66, 0x31, 0xF6, 0x1F,}
-  //,{0x10, 0x14, 0x39, 0x2E, 0xFF, 0xFF, } ejemplo de como autorizar más tarjetas  0x83,.....
-
-};
-
-int i;
-boolean valido = false;
+//Programa
+boolean lectura = false;
+boolean espera = true;
 
 void setup() {
-  Serial.begin(9600);   // Initialize serial communications with the PC
-  
-  pinMode(ledPinVerde  , OUTPUT);
-  pinMode(ledPinAzul, OUTPUT);
-  pinMode(ledPinRojo , OUTPUT);
+  Serial.begin(115200);
+  while (!Serial);
 
-  while (!Serial);      // No hacer nada si no se abre un puerto serie (agregado para Arduinos basado en ATMEGA32U4)
-  SPI.begin();          // I// Comienzo de la comunicación SPI
-  mfrc522.PCD_Init();   // Activación del lector RFID MFRC522
-  mfrc522.PCD_DumpVersionToSerial();  // Muestra detalles de  PCD - MFRC522 Card Reader details
-  //Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
-  Serial.println(F("Pase la tarjeta por el sensor..."));
+  Serial.println("SetUp iniciado");
+  crearLcd();
+  conectarWifi();
+  conectarRFID();
+
+  Serial.println("SetUp listo");
 }
 
+void crearLcd() {
+  //Iniciamos el LDC
+  Serial.println("Iniciando LCD");
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(4, 0);
+  lcd.print("INTERFAZ");
+  lcd.setCursor(4, 1);
+  lcd.print("INICIADA");
 
+  delay(1000);
+}
 
-boolean isAuthorized(byte * serial);
+void conectarWifi() {
+  //Conectamos WiFi
+  Serial.print("Conectando a: ");
+  Serial.println(ssid);
+  lcdArriba("Conectando a: ", 1);
+  lcdAbajo(ssid, 1);
+  delay(1000);
+
+  //Cambiamos a modo Cliente y conectamos a la red
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+  lcdArriba("Conexion", 4);
+  lcdAbajo("Establecida", 3);
+  delay(500);
+}
+
+void conectarRFID() {
+  //Creamos y verificamos RFID
+  SPI.begin();
+  mfrc522.PCD_Init();
+  mfrc522.PCD_DumpVersionToSerial();
+}
+
+void leerRFID() {
+  // Revisamos si hay nuevas tarjetas
+  if ( ! mfrc522.PICC_IsNewCardPresent())
+  {
+    return;
+  }
+  Serial.println("Tarjeta detectada");
+
+  // Leer tarjeta unica
+  if ( ! mfrc522.PICC_ReadCardSerial())
+  {
+    return;
+  }
+  Serial.println("Tarjeta unica puesta");
+
+  //Evitamos lecturas no deseadas
+  if (lectura) {
+    return;
+  }
+
+  String tag = "";
+
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    tag.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    tag.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  tag.toUpperCase();
+
+  if (tag != "") {
+    lectura = true;
+    String tagC = "";
+
+    for(int i = 0; i < tag.length(); i++){
+      if(tag[i] != ' '){
+        tagC += tag[i];
+      }
+    }
+    
+    informarLectura(tagC);
+  }
+}
+
+//Informamos al servidor que se realizó una lectura del RFID
+void informarLectura(String tag) {
+  Serial.print("Tarjeta Detectada:");
+  Serial.println(tag);
+  lcdArriba("TarjetaDetectada", 0);
+  lcdAbajo(tag, 4);
+
+  //Creamos el Host
+  HTTPClient http;
+  String host = "http://192.168.0.11/2101/";
+  host.concat(tag);
+
+  Serial.print("Solicitando: ");
+  Serial.println(host);
+
+  //Realizamos la Solicitud
+  http.begin(host);
+
+  //Capturamos el codigo de la respuesta HTTP
+  int statusCode = http.GET();
+  Serial.print("Status: ");
+  Serial.println(statusCode);
+
+  //Obtenemos la respuesta
+  String res = http.getString();
+
+  Serial.print("Respuesta: ");
+  Serial.println(res);
+
+  //Enviamos la información de la respuesta
+
+  //Colocamos los valores de inicio
+  http.end();
+
+  if (statusCode == 200) {
+    mostrarRespuesta(res);
+  }else{
+    lcdArriba("INTENTE DE NUEVO" , 0);
+    lcdAbajo("" , 0);
+  }
+
+  espera = true;
+  lectura = false;
+}
+
+void mostrarRespuesta(String res) {
+  String nombre = "";
+  String estado = "";
+  boolean switcher = true;
+
+  for (int i = 0; i < res.length(); i++) {
+    if (res[i] == '-') {
+      switcher = false;
+      i++;
+    }
+
+    if (switcher) {
+      nombre += res[i];
+    } else {
+      estado += res[i];
+    }
+  }
+
+  lcdArriba(nombre, 0);
+  lcdAbajo(estado , 0);
+  delay(3000);
+}
+
+void lcdArriba(String msj, int p) {
+  lcd.setCursor(0, 0);
+  lcd.print("                ");
+  lcd.setCursor(p, 0);
+  lcd.print(msj);
+}
+
+void lcdAbajo(String msj, int p) {
+  lcd.setCursor(0, 1);
+  lcd.print("                ");
+  lcd.setCursor(p, 1);
+  lcd.print(msj);
+  delay(1000);
+}
 
 void loop() {
-  // Look for new cards
-  //si no reconoce la conexion
-  if ( ! mfrc522.PICC_IsNewCardPresent()) {
-    digitalWrite(ledPinAzul, HIGH);
-    return;
+  if (espera) {
+    Serial.println("En espera");
+    lcdArriba("COLOQUE SU", 3);
+    lcdAbajo("CARNET", 5);
+    espera = false;
   }
 
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) {
-    digitalWrite(ledPinRojo, HIGH);
-    Serial.print("Codigo: ")
-    // Muestra número de serie leído
-
-    //  Seguir pobando codigo de
-    //https://miarduinounotieneunblog.blogspot.com/2016/02/control-de-acceso-con-el-modulo-rfid.html
-    /*
-    for(int i=0; i<=4 ; i++)
-        {
-          numero_serie[i] = mfrc522.uid.uidByte[i];
-          Serial.print(numero_serie[i]);
-          if(i<=3)
-          {
-            Serial.print("/");
-          }        
-        }            
-        Serial.print(" --> ");
-        delay(500);*/
-    return;
-  }
+  leerRFID();
 }
-
-
-
-
